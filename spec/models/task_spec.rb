2dab657164525d3fbe0e57e5b12182d916a0b2bc
yoshitaka <ymaekawa12.03@gmail.com>
2021-04-19 21:24:32 +0900

    Fix: 変数名を内容がわかるように変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,47 +5,47 @@ RSpec.describe Task, type: :model do
 
   context 'タスクのタイトルが存在しない場合' do
     before do
-      @task = user.tasks.create(title: nil, status: 0)
+      @task_without_title = user.tasks.create(title: nil, status: 0)
     end
 
     it 'バリデーションエラーが発生する' do
-      expect(@task).to be_invalid
+      expect(@task_without_title).to be_invalid
     end
 
     it 'エラーメッセージを返す' do
       @task.valid?
-      expect(@task.errors[:title]).to include("can't be blank")
+      expect(@task_without_title.errors[:title]).to include("can't be blank")
     end
   end
 
   context 'タスクのタイトルが重複している場合' do
     before do
       user.tasks.create(title: 'テストタスク', status: 0)
-      @task2 = user.tasks.build(title: 'テストタスク', status: 1)
+      @task_with_duplicated_title = user.tasks.build(title: 'テストタスク', status: 1)
     end
 
     it 'バリデーションエラーが発生する' do
-      expect(@task2).to be_invalid
+      expect(@task_with_duplicated_title).to be_invalid
     end
 
     it 'エラーメッセージを返す' do
-      @task2.valid?
-      expect(@task2.errors[:title]).to include("has already been taken")
+      @task_with_duplicated_title.valid?
+      expect(@task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
   end
 
   context 'タスクの状態が存在しない場合' do
     before do
-      @task = user.tasks.create(title:'テストタスク', status: nil)
+      @task_without_status = user.tasks.create(title:'テストタスク', status: nil)
     end
 
     it 'バリデーションエラーが発生する' do
-      expect(@task).to be_invalid
+      expect(@task_without_status).to be_invalid
     end
 
     it 'エラーメッセージを返す' do
       @task.valid?
-      expect(@task.errors[:status]).to include("can't be blank")
+      expect(@task_without_status.errors[:status]).to include("can't be blank")
     end
   end
 end
